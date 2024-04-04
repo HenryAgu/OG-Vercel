267c6d7e1d0b2f424bf32fbced7015caab7f40d4
Henry Agu <aguhenrychuks@gmail.com>
2024-04-04 08:47:07 +0100

    fix: removed old meta

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,5 @@
-import { Metadata } from "next";
 import Head from "next/head";
 
-export const metadata: Metadata = {
-  openGraph: {
-    images: [
-      {
-        url: "http://localhost:3000/api/og?title=Home+Page",
-        width: 1200,
-        height: 630,
-        alt: "Home Page",
-      },
-    ],
-  },
-};
 export default function Home() {
   return (
     <div>

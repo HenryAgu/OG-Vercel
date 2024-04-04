31734c1029594de1ea0e3941c8036090ca19900b
Henry Agu <aguhenrychuks@gmail.com>
2024-04-04 10:34:25 +0100

    fix: updated runtime to experimental-edge

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,18 @@ export default function Home() {
   return (
     <div>
       <Head>
-      <title>Home-Page</title>
+        <title>Home-Page</title>
         <meta
           property="og:image"
           content="https://og-vercel-ashy.vercel.app/api/og?title=Home+Page"
         />
+        <meta
+          name="twitter:image"
+          content="https://og-vercel-ashy.vercel.app/api/og?title=Home+Page"
+        />
+        {/* WhatsApp */}
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
       </Head>
       <h1>Home-Page</h1>
     </div>

055253dfc71c5f5d4a758d5a202dbf057081074a
Henry Agu <aguhenrychuks@gmail.com>
2024-04-04 07:50:30 +0100

    feat: static OG-image title

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import { Metadata } from "next";
 
-export const metadata: Metadata={
-  openGraph:{
-    images:[
+export const metadata: Metadata = {
+  openGraph: {
+    images: [
       {
         url: "http://localhost:3000/api/og?title=Home%20Page",
         width: 1200,
@@ -15,6 +15,13 @@ export const metadata: Metadata={
 export default function Home() {
   return (
     <main>
+      <head>
+        <title>Home-Page</title>
+        <meta
+          property="og:image"
+          content="https://og-vercel-ashy.vercel.app/api/og"
+        />
+      </head>
       <h1>Home-Page</h1>
     </main>
   );

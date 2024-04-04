df3b5a00722e475425edfa14964249661f56b8ed
Henry Agu <aguhenrychuks@gmail.com>
2024-04-04 08:23:29 +0100

    fix: added title to og url

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   openGraph: {
     images: [
       {
-        url: "http://localhost:3000/api/og?title=Home%20Page",
+        url: "http://localhost:3000/api/og?title=Home+Page",
         width: 1200,
         height: 630,
         alt: "Home Page",

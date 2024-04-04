70b01a46350aa061bebacefab68c3cb7e8d84d82
Henry Agu <aguhenrychuks@gmail.com>
2024-04-04 07:39:01 +0100

    feat: static OG-image title

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export const metadata: Metadata={
   openGraph:{
     images:[
       {
-        url: "http://localhost:3000/api/og?title=Home Page",
+        url: "http://localhost:3000/api/og?title=Home%20Page",
         width: 1200,
         height: 630,
         alt: "Home Page",
